1b23c4a9f28195c78b3dce8322e2197fc48b190a
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-08-24 12:08:15 -0700

    commit

--- expat-journal/src/components/LandingPage.js
+++ expat-journal/src/components/LandingPage.js
@@ -1,2 +1,45 @@
 // Landing Page.js 
 import React from 'react';
+import React, {useState,useEffect} from "react";
+import './App.css';
+import axios from "axios";
+import LandingPageCard from '../components/LandingPageCard'; 
+import Header from './components/Header';
+
+
+const App = () => {
+
+ 
+  const [data, setData]= useState([]);
+  useEffect( () => {   
+
+    axios.get('http:localhost:3001/landing-articles/')
+    .then( response => {
+      
+      setData(response.data.results); 
+     
+      console.log(response.data.results)
+
+    })  
+    .catch(error => console.log('You got an error.', error))
+  }, []);
+
+ 
+  return ( 
+
+    
+    <div className="App">
+
+    <div className="cards">            
+    
+        {data.map( key=> (<Card key={key.name} props={key}/>))}
+                   
+    </div>  
+
+    </div>
+
+    
+  );
+}
+
+export default App;
--- expat-journal/src/components/LandingPageCard.js
+++ expat-journal/src/components/LandingPageCard.js
@@ -1,1 +1,60 @@
 // Landing Page Card .js
+import React from "react";
+import styled from 'styled-components'; 
+
+const WrapperDiv = styled.div`
+    
+    padding: 10px 10px 10px 10px;
+    width: 300px;
+    height: 150px;
+    margin: 0 auto;
+    background-color: blue;    
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+    align="left";
+
+    box-shadow: 2px 2px 2px 2px #ffff66;
+
+    &:hover {
+
+        box-shadow: 2px 2px 2px 2px red;
+        
+        h1 {
+               color: red;
+        }
+    }
+    
+
+`;
+
+
+const StyledHeading = styled.h1`
+
+    font-size: .6rem;
+    color: white;
+    
+`;
+
+function Card ({props}) {
+    
+    return (
+        
+        
+        <div className="cards">
+            
+            <WrapperDiv>             
+            
+                           
+                <StyledHeading>Birth Year:   {props.birth_year}</StyledHeading>
+                <StyledHeading>Gender:   {props.gender}</StyledHeading>
+                <StyledHeading>Hair Color:   {props.hair_color}</StyledHeading>
+                <StyledHeading>Skin Color:   {props.skin_color}</StyledHeading>
+                <StyledHeading>Mass:   {props.mass}</StyledHeading>  
+            
+            </WrapperDiv>
+    
+        </div>
+        );    
+    
+    };    
