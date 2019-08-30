006da8e3315efd88108a4e4a6e608aaf8347ff8b
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-08-29 21:26:11 -0700

    Adding Index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage';
+import ArticlePage from './components/ArticlePage';
+import { Route,} from 'react-router-dom';
+import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div className='App'>
-      <UserProfile />
+   
+    <div className="App">
+
+    <Header />
+    
+    <Route exact path="/" component={LandingPage} />
+
+    <Route path="/ArticlePage" component={ArticlePage} />
+
+    <Route path="/UserProfile" component={UserProfile} />
+
+    {/* add routing for profile here*/}
+       
     </div>
   );
 }
